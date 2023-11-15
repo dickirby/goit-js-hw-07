ad3bb6edef17e0690107bb88b26255bb7c9f68d6
dickirby <danyasolod2004@gmail.com>
2023-11-15 15:32:43 +0200

    homework done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,63 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems.map(
+  ({ preview, original, description }) => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+  `
+);
+
+gallery.insertAdjacentHTML("beforeend", markup.join(""));
+
+function selectImage(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const original = event.target.dataset.source;
+  const description = event.target.alt;
+
+  const instance = basicLightbox.create(
+    `
+    <img
+      class="gallery__image"
+      src="${original}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  `,
+    {
+      onShow: (instance) => {
+        addEventListener("keydown", function onClose(event) {
+          if (event.key === "Escape") {
+            instance.close();
+          }
+        });
+      },
+      onClose: (instance) => {
+        removeEventListener("keydown", function onClose(event) {
+          if (event.key === "Escape") {
+            instance.close();
+          }
+        });
+      },
+    }
+  );
+
+  instance.show();
+}
+
+gallery.addEventListener("click", selectImage);
